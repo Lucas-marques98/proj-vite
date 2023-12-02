4adcf9dc83577c716d00d08af50065456a9e286f
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2023-12-02 19:40:30 -0300

    Second Commit (rotas de pag e header e footer)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import Header from './components/Header'
+import AppRoutes from './routes'
 
 function App() {
 
   return (
-   <>
-   <Header/>
-   </>
+    <AppRoutes />
   )
 }
 

0045734139f95a081622086a931a47bd7adb9fa6
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2023-12-05 14:46:41 -0300

    Adicionei a page 404

--- src/pages/Contatos/index.jsx
+++ src/pages/Contatos/index.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-
+import Container from '../../components/Container'
 function Contatos() {
   return (
     <>
     <Header />
+    <Container>
     <h1>Contatos</h1>
+    </Container>
     <Footer/>
     </>
   )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,30 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import Container from '../../components/Container'
+import styles from './Home.module.css'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
       <>
       <Header/>
-      <section className='container'>
-     <div className='apresentacao'>
+      <Container>
+      <section className={styles.home}>
+     <div className={styles.apresentacao}>
        <p>
          Olá, sou <br/>
          <span>Lucas Marques</span> <br/>
          Dev Full Stack
-       </p>\
-       <button className='btn btn-red'>
+       </p>
+       <Link to="/sobre"className={`${styles.btn} ${styles.btn_red}`}>
          Saiba mais sobre mim
-       </button>
+       </Link>
      </div>
      <figure>
-       <img className='img-home' src="/developer-red.svg" alt="Imagem de Home" />
+       <img className={styles.img_home} src="/developer-red.svg" alt="Imagem de Home" />
      </figure>
    </section>
+      </Container>
       <Footer/>
       </>
     )
